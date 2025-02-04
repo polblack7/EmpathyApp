95936d16e9682c9764214c489fca206433605348
polblack7 <polblack@bk.ru>
2025-02-04 18:25:39 +0300

    init

---
+++ EmpathyApp/EmpathyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpathyAppApp.swift
+//  EmpathyApp
+//
+//  Created by Nikita Annenkov  on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct EmpathyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmpathyAppTests/EmpathyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmpathyAppTests.swift
+//  EmpathyAppTests
+//
+//  Created by Nikita Annenkov  on 2/4/25.
+//
+
+import Testing
+@testable import EmpathyApp
+
+struct EmpathyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
